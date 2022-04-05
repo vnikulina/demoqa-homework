8c8efbc179a0bc807c7aa114a2a0c45b8e21e227
vnikulina <valeriya.nikulina@gmail.com>
2022-04-05 19:38:46 +0300

    one more try to scrollintoveiw "submit" button

--- src/test/java/PracticeFormTest.java
+++ src/test/java/PracticeFormTest.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -14,15 +15,10 @@ public class PracticeFormTest {
 
     @BeforeAll
     static void beforeAll() {
-        //Configuration.startmaximized = true; // doesn't work -- I have to maximize the browser window manualy to pass the test:(
-        Configuration.browserSize = "1080x1680";
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1080x1920";
         Configuration.browserPosition = "0x0";
         Configuration.baseUrl = "https://demoqa.com";
-    }
-
-    @AfterEach
-    void afterEach() {
-        Selenide.closeWebDriver();
     }
 
     @Test
@@ -36,11 +32,11 @@ public class PracticeFormTest {
         String phone = "92" + Math.round(10000000 + 10000000 * Math.random());
         $("#userNumber").setValue(phone);
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").selectOptionByValue("1986");
+        $(".react-datepicker__year-select").selectOptionByValue("1996");
         $(".react-datepicker__month-select").selectOptionContainingText("June");
         $(".react-datepicker__day--023").click();
         $("#subjectsInput").setValue("Biology").pressEnter();
-        $("#subjectsInput").setValue("Chemistry").pressEnter();
+        $("#subjectsInput").setValue("Physics").pressEnter();
         $(byText("Reading")).click();
         $(byText("Sports")).click();
         File pic = new File("src/test/resourses/test.jpg");
@@ -48,11 +44,12 @@ public class PracticeFormTest {
         $("#currentAddress").sendKeys("Земля");
         $("#react-select-3-input").setValue("NCR").pressEnter();
         $("#react-select-4-input").setValue("Delhi").pressEnter();
-        $("#submit").scrollTo().click();
+        //$("#react-select-4-input").click();
+        $("#submit").scrollIntoView(true).click();
         //Check this form
         $(".modal-header").shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").shouldHave(text("Лера Никулина"),
-                text("test@test.ru"),text("Other"),text(phone),text("23 June,1986"),text("Biology, Chemistry"),
+                text("test@test.ru"),text("Other"),text(phone),text("23 June,1996"),text("Biology, Physics"),
                 text("Reading, Sports"),text("test.jpg"),text("Земля"),text("NCR Delhi")
         );
     }
